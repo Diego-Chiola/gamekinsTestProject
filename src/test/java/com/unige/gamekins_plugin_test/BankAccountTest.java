162cd76c2a65dcb88787997f1fc09c0cf486d5bb
Diego-Chiola <chioladiego@gmail.com>
2023-11-21 15:35:23 +0100

    Corrected test

--- src/test/java/com/unige/gamekins_plugin_test/BankAccountTest.java
+++ src/test/java/com/unige/gamekins_plugin_test/BankAccountTest.java
@@ -1,6 +1,6 @@
 package com.unige.gamekins_plugin_test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,8 +8,9 @@ public class BankAccountTest {
     @Test
     public void BankAccountConstructorWithBalanceNegative(){
         Person person = new Person("Pippo", "Baudo", 18);
-        double expected = -100;
-        BankAccount b = new BankAccount(person, -100);
-        assertEquals(expected, b.getBalance());
+        double balance = -100;
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BankAccount(person, balance);
+        });
     }
 }
