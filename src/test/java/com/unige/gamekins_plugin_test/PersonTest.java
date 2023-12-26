b5e9232605c266b5cae2979ff3b58b4de5a6a652
Diego-Chiola <74010328+Diego-Chiola@users.noreply.github.com>
2023-12-26 19:14:45 +0100

    Update PersonTest.java

--- src/test/java/com/unige/gamekins_plugin_test/PersonTest.java
+++ src/test/java/com/unige/gamekins_plugin_test/PersonTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class PersonTest {
+
+    
     @Test
     public void PersonConstructorTest(){
         Person person = new Person("Diego", "Chiola", 10);
